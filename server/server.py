7d1cb676c04a20f3723d4ea60006635f44bfea01
aoa9 <aoa9@cornell.edu>
2022-06-14 14:35:55 -0500

    Set up environment Variables

--- server/server.py
+++ server/server.py
@@ -1,10 +1,20 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# How to use .env 
+
+GCP_PROJECT_ID = os.getenv('GCP_PROJECT_ID')
+# SERVICE_ACCOUNT_FILE = os.getenv('SERVICE_ACCOUNT_FILE')
+# STORAGE_BUCKET_NAME = os.getenv('STORAGE_BUCKET_NAME')
 
 app = Flask(__name__)
 
 @app.route('/results')
 def index():
-    return 'Hello World'
+    return "Hello World"
 
 
 
